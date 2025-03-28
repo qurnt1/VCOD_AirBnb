6a9a975c56a2eb23309f44ce26b9437020174689
Quentin CHABOT <quentin.chabot@etu.univ-poitiers.fr>
2025-03-28 21:22:19 +0100

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,13 @@
+# Créer les fichiers principaux
+touch main.py
+touch requirements.txt
+
+# Créer le dossier datas et ses sous-dossiers
+mkdir -p datas/data
+mkdir -p datas/imgs
+
+# Ajouter les fichiers dans les bons dossiers (exemple ici)
+touch datas/data/monuments.csv
+touch datas/data/listings.csv
+touch datas/data/neighbourhoods.geojson
+touch datas/imgs/logo.png
